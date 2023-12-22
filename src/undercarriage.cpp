1cc852bbb8d0076aeba9878c173332360ab2e481
Konnyaku <mihiro.furuya@gmail.com>
2023-12-22 09:49:13 +0900

    シラス用のCAN送信関数を作成

--- src/undercarriage.cpp
+++ src/undercarriage.cpp
@@ -3,8 +3,9 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "undercarriage.hpp"
+#include "harurobo/undercarriage.hpp"
 #include <can_utils.hpp>
+#include <send_data.hpp>
 
 using std::placeholders::_1;
 
@@ -23,7 +24,7 @@ public:
 private:
   void topic_callback(const sensor_msgs::msg::Joy & msg)
   {  
-    if(msg.buttons[6]){
+    if(msg.buttons[6]){//startボタン
         auto RF_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_front_motor),0x5);
         auto LF_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_back_motor),0x5);
         auto RB_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::left_front_motor),0x5);
@@ -32,8 +33,8 @@ private:
         can_pub_->publish(std::move(RB_mode_frame));
         can_pub_->publish(std::move(LF_mode_frame));
         can_pub_->publish(std::move(LB_mode_frame));
-    }
-    if(msg.buttons[4]){
+    }//mode velにする
+    if(msg.buttons[4]){//backボタン
         auto RF_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_front_motor),0x0);
         auto LF_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_back_motor),0x0);
         auto RB_mode_frame = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::left_front_motor),0x0);
@@ -42,21 +43,21 @@ private:
         can_pub_->publish(std::move(RB_mode_frame));
         can_pub_->publish(std::move(LF_mode_frame));
         can_pub_->publish(std::move(LB_mode_frame));
-    }
+    }//mode disにする
     // RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg.buttons[0]);
-    if(msg.buttons[9]){
+    if(msg.buttons[9]){//ZL(left shouldderボタン)
         this->koinobori.update(msg.axes[0],msg.axes[1],turn_direction::left_turn);
-    }
-    else if(msg.buttons[10]){
+    }//left turn
+    else if(msg.buttons[10]){//ZR(right shouldderボタン)
         this->koinobori.update(msg.axes[0],msg.axes[1],turn_direction::right_turn);
-    }
+    }//right turn
     else{
         this->koinobori.update(msg.axes[0],msg.axes[1],turn_direction::no_turn);
-    }
-    auto RFframe = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_front_motor)+1,koinobori.get_TARGET(motor_name::right_front_motor));
-    auto RBframe = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::right_back_motor)+1,koinobori.get_TARGET(motor_name::right_back_motor));
-    auto LFframe = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::left_front_motor)+1,koinobori.get_TARGET(motor_name::left_front_motor));
-    auto LBframe = can_utils::generate_frame(koinobori.get_CAN_ID(motor_name::left_back_motor)+1,koinobori.get_TARGET(motor_name::left_back_motor));
+    }//平行移動
+    auto RFframe = can_utils::shirasu_target(koinobori.get_CAN_ID(motor_name::right_front_motor)+1,koinobori.get_TARGET(motor_name::right_front_motor));
+    auto RBframe = can_utils::shirasu_target(koinobori.get_CAN_ID(motor_name::right_back_motor)+1,koinobori.get_TARGET(motor_name::right_back_motor));
+    auto LFframe = can_utils::shirasu_target(koinobori.get_CAN_ID(motor_name::left_front_motor)+1,koinobori.get_TARGET(motor_name::left_front_motor));
+    auto LBframe = can_utils::shirasu_target(koinobori.get_CAN_ID(motor_name::left_back_motor)+1,koinobori.get_TARGET(motor_name::left_back_motor));
     can_pub_->publish(std::move(RFframe));
     can_pub_->publish(std::move(RBframe));
     can_pub_->publish(std::move(LFframe));
